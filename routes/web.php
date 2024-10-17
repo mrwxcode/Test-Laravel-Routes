e38a539f70bf36c08b47eb7e568a1c57999bf504
Wen Xuan Goh <wenxuan.goh@v6technology.net>
2024-10-17 16:43:42 +0800

    laravel-tests

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
-use App\Http\Controllers\Admin\StatsController as AdminStatsController;
+use App\Http\Controllers\Admin\StatsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,13 +26,13 @@ Route::get('/', [HomeController::class, 'index']);
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
-Route::get('users/{name}', [UserController::class, 'show']);
+Route::get('/users/{name}', [UserController::class, 'show']);
 
 // Task 3: point the GET URL "/about" to the view
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-Route::view('/about', 'page.about')->name('about');
+Route::view('/about', 'pages.about')->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
@@ -75,11 +74,11 @@ Route::group(['middleware' => 'auth'], function(){
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::view('/dashboard', [AdminDashboardController::class]);
+        Route::get('/dashboard', [AdminDashboardController::class]);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-        Route::view('/stats', [AdminStatsController::class]);
+        Route::get('/stats', [StatsController::class]);
 
     });
     // End of the /admin Route Group
